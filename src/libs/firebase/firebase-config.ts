80ec249153acfcb9e64c8cc2df7ac673a1f1b1cf
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-06-20 15:36:40 +0900

    add jsonファイルからfirebaseのdbにドキュメントを追加できる機能

--- src/libs/firebase/firebase-config.ts
+++ src/libs/firebase/firebase-config.ts
@@ -11,6 +11,17 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+// Node.jsでプログラムを実行するとき用
+// dotenv.config();
+// const firebaseConfig = {
+//   apiKey: process.env.VITE_FIREBASE_API_KEY!,
+//   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN!,
+//   projectId: process.env.VITE_FIREBASE_PROJECT_ID!,
+//   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET!,
+//   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID!,
+//   appId: process.env.VITE_FIREBASE_APP_ID!,
+// };
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
